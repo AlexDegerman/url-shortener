3f0a4f56b529e4bcc171e5773e5f9dee22510b37
AlexDegerman <alex.degerman.dev@gmail.com>
2022-07-09 02:11:01 +0300

    add basic url shortener

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { UrlShortenerComponent } from './url-shortener/url-shortener.component';
+import {NgTinyUrlModule} from 'ng-tiny-url';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    UrlShortenerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgTinyUrlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/url-shortener/url-shortener.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {NgTinyUrlService} from 'ng-tiny-url';
+
+@Component({
+  selector: 'app-url-shortener',
+  templateUrl: './url-shortener.component.html',
+  styleUrls: ['./url-shortener.component.css']
+})
+export class UrlShortenerComponent implements OnInit {
+  url: any;
+  shortenedUrl: any;
+  submission = true;
+  results = false;
+  constructor(private tinyUrl: NgTinyUrlService) { }
+
+  ngOnInit(): void {
+  }
+
+  shortenUrl(){
+    this.tinyUrl.shorten(this.url).subscribe(result => {
+      this.shortenedUrl = result;
+      this.submission = false;
+      this.results = true;
+    })
+  }
+}
